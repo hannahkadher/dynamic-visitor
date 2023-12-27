8bb5da76087bec12a3ec09b2c7a123ca32fc55c4
hannath <hannathabdulkadher@gmail.com>
2023-12-27 13:40:00 +0400

    Defined app module and mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FormModule } from './form/form.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/dynamic-visitor-forms'),
+    FormModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
