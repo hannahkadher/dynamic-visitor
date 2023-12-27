6beb7058a2963df1b4f3cbef0c02d8546586603a
hannath <hannathabdulkadher@gmail.com>
2023-12-27 13:40:25 +0400

    added controller route constant

---
+++ src/constants/controller-route.ts
@@ -0,0 +1,12 @@
+// constants.ts
+
+/**
+ * Defines constant values related to controller routes.
+ */
+export class ControllerRoute {
+    /**
+     * Represents the route for form-related controllers.
+     * Usage: ControllerRoute.FORM_ROUTE
+     */
+    public static readonly FORM_ROUTE = 'form';
+}
