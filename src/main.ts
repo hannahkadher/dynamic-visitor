520a8dc2fec098010a764dec30d5b5aba0db230d
hannath <hannathabdulkadher@gmail.com>
2023-12-27 13:40:12 +0400

    added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Dynamic Visitor App')
+  .setDescription('Service for creation of dynamic visitor form and accept data to the form')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
